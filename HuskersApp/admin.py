8e2cefcbff5b641116cd85f3dadcefd3e7720ef1
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-03-15 16:09:09 -0500

    Added Models, Docker files and updated Readme

--- HuskersApp/admin.py
+++ HuskersApp/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Group, Venue, Post
+
 
 # Register your models here.
+
+
+class VenueList(admin.ModelAdmin):
+    list_display = ('name', 'address_line_1', 'address_line_2', 'city', 'state', 'country')
+    list_filter = ('name', 'city', 'state')
+    search_fields = ('name', 'city', 'state')
+
+
+class GroupList(admin.ModelAdmin):
+    list_display = ('name', 'venue', 'meeting_time', 'groupAdmin', 'hashtag',)
+    list_filter = ('name', 'venue', 'meeting_time')
+    search_fields = ('name', 'venue')
+
+
+class PostList(admin.ModelAdmin):
+    list_display = ('post_text', 'group', 'user')
+    list_filter = ('group',)
+    search_fields = ('group',)
+
+
+admin.site.register(Group, GroupList)
+admin.site.register(Venue, VenueList)
+admin.site.register(Post, PostList)
+
--- HuskersApp/models.py
+++ HuskersApp/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Models for Huskers Network
+
+class Venue(models.Model):
+    name = models.CharField(max_length=200)
+    address_line_1 = models.CharField(max_length=500)
+    address_line_2 = models.CharField(max_length=500)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+    venue = models.ForeignKey(Venue, related_name='group_venue')
+    meeting_time = models.TimeField(auto_now=True)
+    groupAdmin = models.ForeignKey(User, related_name='group_admin')
+    hashtag = models.CharField(max_length=100)
+    users = models.ManyToManyField(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Post(models.Model):
+    post_text = models.TextField(max_length=None)
+    group = models.ForeignKey(Group, related_name='group_post', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_post')
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.post_text
+
+
+
+
+
