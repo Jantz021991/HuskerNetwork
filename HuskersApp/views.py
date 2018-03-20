ca69612e2934e5ce47a881677f4572daf29c1f6e
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-03-20 12:33:15 -0500

    Send Data to Home page

--- HuskersApp/views.py
+++ HuskersApp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
+    posts = Post.objects.count()
+    groups = Group.objects.count()
+    members = User.objects.count()
     return render(request, 'HuskersApp/home.html',
-                  {'HuskersApp': home})
+                  {'HuskersApp': home,
+                  'posts': posts,
+                  'groups': groups,
+                  'members': members})
