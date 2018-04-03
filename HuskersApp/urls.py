b4a709c9323193a8a2fd38f8746d202bfc803bde
Vivek Bhat <vbhathosmat@unomaha.edu>
2018-04-02 19:01:55 -0500

    added venues

--- HuskersApp/urls.py
+++ HuskersApp/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^group/', views.group, name='group'),
+    url(r'^venue/', views.venue, name='venue'),
+    url(r'^feed/', views.feed, name='feed'),
+    url(r'^venue_detail/', views.venue_detail, name='venue_detail'),
 ]
 
--- HuskersApp/views.py
+++ HuskersApp/views.py
@@ -6,3 +6,22 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'HuskersApp/home.html',
                   {'HuskersApp': home})
+
+
+def group(request):
+    return render(request, 'HuskersApp/group.html',
+                  {'HuskersApp': group})
+
+
+def venue(request):
+    return render(request, 'HuskersApp/venue.html',
+                  {'HuskersApp': venue})
+
+def feed(request):
+    return render(request, 'HuskersApp/feed.html',
+                  {'HuskersApp': feed})
+
+
+def venue_detail(request):
+    return render(request, 'HuskersApp/venue_detail.html',
+                  {'HuskersApp': venue_detail})
