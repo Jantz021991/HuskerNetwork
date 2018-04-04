640e5639fd8e8a71d60169927ed2f58cba9dd2bb
Vaibhav Rahangdale <vrahangdale@unomaha.edu>
2018-04-04 17:40:55 -0500

    updated venue_details

--- HuskersApp/views.py
+++ HuskersApp/views.py
@@ -40,11 +40,11 @@ def feed(request):
 
 def venue_detail(request):
     return render(request, 'HuskersApp/venue_detail.html',
-                  {'HuskersApp': venue_detail},
-                  {'HuskersApp': home,
-                  'posts': posts,
-                  'groups': groups,
-                  'members': members})
+                  {'HuskersApp': venue_detail})
+                  # {'HuskersApp': home,
+                  # 'posts': posts,
+                  # 'groups': groups,
+                  # 'members': members})
 
 """
 Manage Venues
