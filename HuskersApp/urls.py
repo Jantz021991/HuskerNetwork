46a67de321eff0ce4de769b8012db5da290dcef2
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-04-04 16:42:27 -0500

    Redirected to proper group page and added css to create forms

--- HuskersApp/urls.py
+++ HuskersApp/urls.py
@@ -5,7 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^group/', views.group, name='group'),
+    # url(r'^group/', views.group, name='group'),
     url(r'^venue/', views.venue, name='venue'),
     url(r'^feed/', views.feed, name='feed'),
     url(r'^venue_detail/', views.venue_detail, name='venue_detail'),
