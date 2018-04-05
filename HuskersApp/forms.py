214c898902478964d1990e7d6156c5788f3858ef
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-04-05 12:05:56 -0500

    Revert "Changes for User Registration"
    This reverts commit 62f101bc9aef8abef537bc479dad44c429476fb3.

--- HuskersApp/forms.py
+++ HuskersApp/forms.py
@@ -21,24 +21,25 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('post_text', 'group', 'user')
 
-
+<<<<<<< HEAD
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
-
+=======
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=60, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+>>>>>>> cb76cc7b46e5a7b773e64671fa27aae82a51c4cf
 
     class Meta:
         model = User
         #From User fields
         fields = ('username', 'first_name', 'email', 'password1', 'password2', )
 
-    def clean_password2(self):
-        '''Check if both password matches'''
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
-        return cd['password2']
+    # def clean_password2(self):
+    #     '''Check if both password matches'''
+    #     cd = self.cleaned_data
+    #     if cd['password'] != cd['password2']:
+    #         raise forms.ValidationError('Passwords don\'t match.')
+    #     return cd['password2']
